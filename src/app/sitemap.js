1dfeefdc77e69a3ff2ad4d0e8a435bea347f40ee
Developer Shyam <shyammishra5857@gmail.com>
2024-10-21 14:04:46 +0530

    ddd

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,30 +1,30 @@
 const { ENDPOINT } = require("@/constant");
 
-async function blogList() {
-  try {
-    const response = await fetch(`${ENDPOINT}/api/blogs`);
+// async function blogList() {
+//   try {
+//     const response = await fetch(`${ENDPOINT}/api/blogs`);
 
-    // Check if the response is not okay (status not in the range 200-299)
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+//     // Check if the response is not okay (status not in the range 200-299)
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
 
-    const data = await response.json(); // Parse JSON response
-    return data.res;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    throw error; // Rethrow the error after logging
-  }
-}
+//     const data = await response.json(); // Parse JSON response
+//     return data.res;
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     throw error; // Rethrow the error after logging
+//   }
+// }
 
 export default async function sitemap() {
-  const blogs = await blogList();
-  const blogsEntries = blogs.map((item) => {
-    return {
-      url: `${ENDPOINT}/blog/${item?.slug}`,
-      lastModified: new Date(item.time?.seconds * 1000),
-    };
-  });
+  // const blogs = await blogList();
+  // const blogsEntries = blogs.map((item) => {
+  //   return {
+  //     url: `${ENDPOINT}/blog/${item?.slug}`,
+  //     lastModified: new Date(item.time?.seconds * 1000),
+  //   };
+  // });
 
   return [
     {
@@ -47,6 +47,6 @@ export default async function sitemap() {
       url: `${ENDPOINT}/blog`,
       lastModified: "2023-11-30T18:30:00.000Z",
     },
-    ...blogsEntries,
+    // ...blogsEntries,
   ];
 }
