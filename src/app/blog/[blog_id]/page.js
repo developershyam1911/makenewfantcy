e0194579f02f58e39d9dd3bb5f750c98fe820e88
Developer Shyam <shyammishra5857@gmail.com>
2024-10-21 13:38:54 +0530

    done

--- src/app/blog/[blog_id]/page.js
+++ src/app/blog/[blog_id]/page.js
@@ -2,36 +2,36 @@ import SingleBlog from "@/components/SingleBlog";
 import { ENDPOINT } from "@/constant";
 import axios from "axios";
 
-export async function generateMetadata({ params }) {
-  const { slug } = params;
-  try {
-    const response = await axios.get(`${ENDPOINT}/api/blogs/${slug}`);
-    if (response.status !== 200) {
-      throw new Error("Network response was not ok.");
-    }
-    const item = response.data.res;
-    console.log(item);
+// export async function generateMetadata({ params }) {
+//   const { slug } = params;
+//   try {
+//     const response = await axios.get(`${ENDPOINT}/api/blogs/${slug}`);
+//     if (response.status !== 200) {
+//       throw new Error("Network response was not ok.");
+//     }
+//     const item = response.data.res;
+//     console.log(item);
 
-    return {
-      title: item[0]?.title,
-      description: item[0]?.desc,
-      keywords: [item[0]?.keyword.split(",")],
-      icons: {
-        icon: "/images/favicon.png",
-        shortcut: "/images/favicon.png",
-        apple: "/images/favicon.png",
-      },
-      openGraph: {
-        title: item[0]?.title,
-        description: item[0]?.desc,
-        images: item[0]?.image,
-      },
-    };
-  } catch (error) {
-    console.error("Error fetching Data:", error);
-    return [];
-  }
-}
+//     return {
+//       title: item[0]?.title,
+//       description: item[0]?.desc,
+//       keywords: [item[0]?.keyword.split(",")],
+//       icons: {
+//         icon: "/images/favicon.png",
+//         shortcut: "/images/favicon.png",
+//         apple: "/images/favicon.png",
+//       },
+//       openGraph: {
+//         title: item[0]?.title,
+//         description: item[0]?.desc,
+//         images: item[0]?.image,
+//       },
+//     };
+//   } catch (error) {
+//     console.error("Error fetching Data:", error);
+//     return [];
+//   }
+// }
 
 export async function generateStaticParams() {
   try {
